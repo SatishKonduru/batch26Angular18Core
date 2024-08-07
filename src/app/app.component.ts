08748865f0d74f929c8500578d07cf6a9ebc2108
satish <satishkonduru0225@gmail.com>
2024-08-07 08:35:20 +0530

    Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from './components/header/header.component';
+
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  preserveWhitespaces: true
+  preserveWhitespaces: true 
 })
 export class AppComponent {
   title = 'batch26Angular18Core';
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, MatToolbarModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
