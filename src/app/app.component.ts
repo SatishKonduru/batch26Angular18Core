7220688b5e4f644a134702c2c1a5b4f0fd06f80a
satish <satishkonduru0225@gmail.com>
2024-08-10 09:15:00 +0530

    Style and Class Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import {MatButtonModule} from '@angular/material/button';
 import { HeaderComponent } from './components/header/header.component';
 import { InterpolationComponent } from "./components/interpolation/interpolation.component";
 import { PropertyBindingComponent } from './components/property-binding/property-binding.component';
+import { StyleBindingComponent } from "./components/style-binding/style-binding.component";
+import { ClassBindingComponent } from "./components/class-binding/class-binding.component";
 
 
 
 @Component({
   selector: 'app-root', 
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, HeaderComponent, InterpolationComponent, PropertyBindingComponent],
+  imports: [RouterOutlet, MatButtonModule, HeaderComponent, InterpolationComponent, PropertyBindingComponent, StyleBindingComponent, ClassBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   preserveWhitespaces: true 
