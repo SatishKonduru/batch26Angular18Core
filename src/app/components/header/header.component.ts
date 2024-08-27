33cc74b27cacb6423238eea7e6815ebd8951d085
satish <satishkonduru0225@gmail.com>
2024-08-27 09:28:08 +0530

    fragments

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,6 +13,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css',
   preserveWhitespaces: true
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  activatedRoute = inject(ActivatedRoute)
+
+  ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe(data => {
+      console.log(data)
+      this.jumpToSection(data)
+    })
+  }
+
+  jumpToSection(section: any){
+    document.getElementById(section).scrollIntoView({behavior: 'smooth'})
+  }
 
 }
